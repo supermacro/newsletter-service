f81f551de47b0b65c8c6d37acbc2416a03e8cfc1
Giorgio Delgado <hi@gdelgado.ca>
2020-12-04 13:02:32 -0500

    Implement unsubscribe endpoint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,7 @@ import * as express from 'express'
 import * as cors from 'cors'
 import * as helmet from 'helmet'
 import subscribeHandler from './routes/subscribe'
-
+import unsubscribeHandler from './routes/unsubscribe'
 
 
 const app = express()
@@ -13,6 +13,8 @@ app.use(helmet())
 app.use(express.json())
 
 app.post('/subscribe', subscribeHandler)
+app.post('/unsubscribe', unsubscribeHandler)
+
 
 // Newsletter Service Entry Point
 export const newsletter = functions.https.onRequest(app);
---
+++ functions/src/routes/unsubscribe.ts
@@ -0,0 +1,9 @@
+import { unsubscribeUserFromMailingList } from '../email'
+import { intoHandlerResult, emailValidator } from '../utils'
+import { createRoute } from '../router'
+
+export default createRoute(emailValidator, (email) =>
+  unsubscribeUserFromMailingList(email)
+    .then((intoHandlerResult))
+)
+
