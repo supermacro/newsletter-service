8b5e0189b5f3b4a84bcddc3c12bbb6d7ec241fd4
Giorgio Delgado <hi@gdelgado.ca>
2021-02-27 14:53:16 -0500

    Send to real subscribers on testing list only

--- functions/src/email.ts
+++ functions/src/email.ts
@@ -13,6 +13,9 @@ import {
 
 export type Newsletter = 'personal' | 'parlezvous' | 'testnewsletter'
 
+const personalList = `${PERSONAL_NEWSLETTER_NAME}@${PERSONAL_EMAIL_SENDER_DOMAIN}`
+const parlezVousList = `${PV_NEWSLETTER_NAME}@${PV_EMAIL_SENDER_DOMAIN}`
+const testingList = `${TEST_NEWSLETTER_NAME}@${PERSONAL_EMAIL_SENDER_DOMAIN}`
 
 const createEndpoint = (path: string) =>
   `https://api.mailgun.net/v3/${path}`
@@ -80,7 +83,7 @@ const createMailgunApiData = (op: Operation): MailgunApiData => {
 
       if (op.testMode) {
         form.append('o:testmode', 'yes')
-      } else {
+      } else if (listName !== testingList) {
         throw new Error('NOT READY FOR THE BIG SHOW')
       }
 
@@ -113,11 +116,6 @@ const createRequest = (op: Operation): Promise<EmailApiOutcome> => {
 
 
 const getMailingListFromVal = (val?: unknown): MailingListInfo => {
-  const personalList = `${PERSONAL_NEWSLETTER_NAME}@${PERSONAL_EMAIL_SENDER_DOMAIN}`
-  const parlezVousList = `${PV_NEWSLETTER_NAME}@${PV_EMAIL_SENDER_DOMAIN}`
-  const testingList = `${TEST_NEWSLETTER_NAME}@${PERSONAL_EMAIL_SENDER_DOMAIN}`
-
-
   if (val === 'parlezvous') {
     return { listName: parlezVousList, senderDomain: PV_EMAIL_SENDER_DOMAIN }
   }
